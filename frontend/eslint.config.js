3a3823d662cb0ced96b410173042bb0bde8f094e
Marc Sancho <marc.catlinux@gmail.com>
2026-09-18 08:31:13 +0200

    Configurar ESLint para Vue con TypeScript

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,5 +1,6 @@
 import js from "@eslint/js";
 import vue from "eslint-plugin-vue";
+import tsParser from "@typescript-eslint/parser";
 
 export default [
   {
@@ -7,4 +8,12 @@ export default [
   },
   js.configs.recommended,
   ...vue.configs["flat/essential"],
+  {
+    files: ["**/*.vue"],
+    languageOptions: {
+      parserOptions: {
+        parser: tsParser,
+      },
+    },
+  },
 ];
