c790526d82e0729e8d4a792d896b7a597a31c460
Marc Sancho <marc.catlinux@gmail.com>
2026-09-18 09:56:12 +0200

    Separa imports de terceros y del proyecto en Alembic

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -5,6 +5,7 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 from sqlmodel import SQLModel
+
 import app.models  # noqa: F401
 from app.db import resolved_database_url
 
